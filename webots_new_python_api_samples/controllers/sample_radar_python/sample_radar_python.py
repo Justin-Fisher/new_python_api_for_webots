924232f6a10c113ade801bff36295a0e51cd2748
TelosTelos <77085563+TelosTelos@users.noreply.github.com>
2022-06-03 02:35:05 -0500

    Radar spotlight improvements

--- webots_new_python_api_samples/controllers/sample_radar_python/sample_radar_python.py
+++ webots_new_python_api_samples/controllers/sample_radar_python/sample_radar_python.py
@@ -20,9 +20,9 @@
 import robot
 from vectors import Pair  # convenient container for managing a left-right pair of similar devices, like Motors
 
-SPEED = 6
-TIME_STEP = 64
-EXPECTED_NUMBER_OF_TARGETS = 3
+SPEED = 6                       # Maximum speed robots will typically move
+TIME_STEP = 64                  # How many milliseconds between each time the controller issues commands
+EXPECTED_NUMBER_OF_TARGETS = 3  # The maximum number of spotlights we can have at once
 
 # === Initialize Devices ===
 
@@ -38,39 +38,41 @@ ds = Pair(robot.DistanceSensor) # now we can address these as ds.left and ds.rig
 
 radar = robot.Radar()  # will be None if the robot has no radar; only the grey-capped robot has radar
 
-# === Create spotlight markers to mark detected radar targets with ===
-if radar is not None:
+# === Create spotlights to mark detected radar targets with ===
+if radar is not None:  # Only the gray-capped robot will do this part
     import world  # so we can use supervisor powers to show where detected radar targets appear to be
-    plan = world.plan  # for ease of reference
-    # If the robot already has a RADAR_MARKERS group, empty it; otherwise create one
-    if world.self.get_node('RADAR_MARKERS'):  # returns None if there is no such node
-        world.self.RADAR_MARKERS.children.clear()
+    # If the robot already has a SPOTLIGHTS group, empty it; otherwise create one
+    if world.self.get_node('SPOTLIGHTS'):  # returns None if there is no such node
+        world.self.SPOTLIGHTS.children.clear()
     else:
-        world.self.children.append(plan.Group(DEF = "RADAR_MARKERS"))
-    markers = world.self.RADAR_MARKERS.children  # the field in which the markers will live
-    # We'll plan an inverted transparent glowing yellow cones to give a spotlight appearance on detected radar targets
-    # The cones are invisibly attached to our robot, making frame-of-reference easy, but will jiggle as it does
-    cone_color = (1,0.8,0)
-    planned_cone = plan.Cone(height=0.8, bottomRadius=0.07,
-                             baseColor=cone_color, emissiveColor = cone_color, transparency = 0.7,
-                             translation=(0,0,-30), rotation=(1,0,0,3.1416))
-    # Create as many cones as we expect to use; unused ones will spend their time hidden deep underground, z = -30
+        world.self.children.append(world.plan.Group(DEF = "SPOTLIGHTS"))
+    spotlights = world.self.SPOTLIGHTS.children  # the field in which the spotlights will live
+    # We'll plan a transparent glowing yellow cone to give a simple spotlight appearance on detected radar targets
+    # The cones are invisibly attached to our robot, making frame-of-reference easy, but will jiggle as our robot does
+    cone_color = (1, 0.8, 0)  # yellow
+    planned_spotlight = world.plan.Cone(translation=(0,0,-30), height=0.4, bottomRadius=0.08,
+                                        baseColor=cone_color, emissiveColor = cone_color, transparency = 0.7)
+    # Create as many spotlights as we expect to use; unused ones will spend their time high in the air
     for i in range(EXPECTED_NUMBER_OF_TARGETS):
-        markers.append(planned_cone)
+        spotlights.append(planned_spotlight)
 
 # === Main loop ===
 while robot.step(TIME_STEP):
     # --- Main loop: Highlight radar targets (done only by the gray-capped robot) ---
     if radar is not None:
-        for m in markers: m.translation = (0, 0, -30) # tentatively hide all radar markers deep below the ground
-        # for each radar target, we'll move a yellow spotlight onto that target to highlight that we can see it
-        for i, target in enumerate(radar):
-            # Start with a vector pointing straight ahead the distance of the detected target
-            pos = robot.Vector(target.distance,0,0)
-            # setting a vector's .angle twists it to that new angle, keeping its magnitude the same
+        # For each radar target, we'll move a yellow spotlight onto that target to highlight that radar can see it
+        for target, spotlight in zip(radar, spotlights):
+            # Start with a vector pointing straight ahead (along x=forward axis) the distance of the detected target
+            pos = robot.Vector(target.distance, 0, 0)
+            # Setting a Vector's .angle twists it to that new angle, keeping its magnitude the same
             pos.angle = -target.azimuth  # negative since this radar is mounted to give -azimuth in the +y direction
-            markers[i].translation = pos  # store this pos as the new translation for this marker in the simulation
+            pos.z += 0.2  # Hover spotlight up above the target (+z is up)
+            spotlight.translation = pos  # store this pos as the new translation for this marker in the simulation
+            # The following command would spam the console with radar sightings, redundant with the moving spotlights
             # print( f"{robot.name} target #{i}: distance={target.distance}, azimuth={target.azimuth}")
+        # Hide the unused spotlights deep underground (simpler and faster than repeatedly deleting and respawning them)
+        for i in range(len(radar), EXPECTED_NUMBER_OF_TARGETS):
+            spotlights[i].translation = (0, 0, -30)
 
     # --- Main loop: Avoid obstacles (done by all the robots) ---
     # The lookup tables on these distance sensors make them read 1024 for very close objects, and less for further ones,
